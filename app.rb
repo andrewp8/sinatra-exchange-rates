64da9eb4d06721c2121d5571ad58af352b577a03
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2023-11-22 22:59:11 +0000

    SUCCEED creating dynamic links for each currency

--- app.rb
+++ app.rb
@@ -34,7 +34,10 @@ get("/:from_currency") do
   api_url = "#{base_URL}live?access_key=#{exchange_rate_API}&source=#{@original_currency}"
 
   # some more code to parse the URL and render a view template
-  @raw_data = HTTP.get(api_url)
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @to_symbols = parsed_data["quotes"]
 
   erb(:from_currency)
 end
@@ -43,7 +46,7 @@ get("/:from_currency/:to_currency") do
   @original_currency = params.fetch("from_currency")
   @destination_currency = params.fetch("to_currency")
 
-  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  api_url = "#{base_URL}convert?access_key=#{exchange_rate_API}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
   
   # some more code to parse the URL and render a view template
 end
